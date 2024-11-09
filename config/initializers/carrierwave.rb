7a4a55039a066ce397597861d4223cfdc115c7a9
稲田鞠子 <inadamariko@inadamarikononotobukkukonpyuta.local>
2024-11-10 08:41:20 +0900

    HobbyImageUploaderclass storage fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,17 @@
 require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
-      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      region:                Rails.application.credentials.dig(:aws, :region),
-    }
-    config.fog_directory  = Rails.application.credentials.dig(:aws, :bucket)
-    config.fog_public     = false
+    if Rails.env.production? || Rails.env.staging?
+        config.storage = :fog
+        config.fog_credentials = {
+        provider:              'AWS',
+        aws_access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
+        aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+        region:                Rails.application.credentials.dig(:aws, :region),
+        }
+        config.fog_directory  = Rails.application.credentials.dig(:aws, :bucket)
+        config.storage = :fog
+    else
+        config.storage = :file
+        config.enable_processing = Rails.env.development?
+    end
 end
