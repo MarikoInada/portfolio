c08a7f2cf91823a9d93eb6615287dd258a8ca913
稲田鞠子 <inadamariko@inadamarikononotobukkukonpyuta.local>
2024-11-10 09:28:44 +0900

    ACL surport errar

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
     if Rails.env.production? || Rails.env.staging?
-        config.storage = :fog
+        config.fog_provider = 'fog/aws'
         config.fog_credentials = {
         provider:              'AWS',
         aws_access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
@@ -10,6 +10,7 @@ CarrierWave.configure do |config|
         }
         config.fog_directory  = Rails.application.credentials.dig(:aws, :bucket)
         config.storage = :fog
+        config.fog_public = false
     else
         config.storage = :file
         config.enable_processing = Rails.env.development?
