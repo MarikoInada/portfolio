b236faad3751687a8058cd10483c8c57d39dd851
稲田鞠子 <inadamariko@inadamarikononotobukkukonpyuta.local>
2024-11-10 10:02:17 +0900

    Fix carrierwave configuration for ActiveSupport::Deprecation issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,6 @@
 require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
     if Rails.env.production? || Rails.env.staging?
-        config.fog_provider = 'fog/aws'
         config.fog_credentials = {
         provider:              'AWS',
         aws_access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
