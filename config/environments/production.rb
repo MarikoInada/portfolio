18528e7870758f28ae16fd8a70b5245485880da2
稲田鞠子 <inadamariko@inadamarikononotobukkukonpyuta.local>
2024-11-14 10:51:36 +0900

    Add precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,6 @@ Rails.application.configure do
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.digest = true
   config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
